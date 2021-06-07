4d757a40b08627aa51671be991744c65879d4b16
WORUS <aswoks@gmail.com>
2021-06-07 16:39:48 +0400

    add project

---
+++ aspmvc/Models/BookDbInitializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace aspmvc.Models
+{
+    public class BookDbInitializer : DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext db)
+        {
+            db.Books.Add(new Book { Name = "Война и мир", Author = "Л. Толстой", Price = 220 });
+            db.Books.Add(new Book { Name = "Отцы и дети", Author = "И. Тургенев", Price = 180 });
+            db.Books.Add(new Book { Name = "Чайка", Author = "А. Чехов", Price = 150 });
+            db.Books.Add(new Book { Name = "Муму", Author = "И. Тургенев", Price = 210} );
+
+            base.Seed(db);
+        }
+    }
+}
